f3637c9b9689fccb8ec36302ef1121240d8ee62f
Chiu-Feng (Steph) Yap <chiufengyap@gmail.com>
2021-11-13 12:59:51 -0600

    updated with more info
    - added an image on the sidebar
    - updated project overview, dataset
    - added 'The Team' with LinkedIn profile links

--- nsduh_streamlit_app.py
+++ nsduh_streamlit_app.py
@@ -18,101 +18,180 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 ######### IMPORT DATA #########
 @st.cache(suppress_st_warning=True,allow_output_mutation=True)
-def import_data():
-    nsduh = pd.read_csv('./Data/nsduh_data_cleaned.csv')
-    return nsduh
+def import_nsduh():
+	nsduh = pd.read_csv('Data/nsduh_data_cleaned.csv')
+	# hpsa = pd.read_csv('Data/HPSA_Cleaned.csv')
+	return nsduh
 
+nsduh = import_nsduh()
 
-df = import_data()
+def import_hpsa():
+	hpsa = pd.read_csv('Data/HPSA_Cleaned.csv')
+	return hpsa
+
+hpsa = import_hpsa()
+
+def import_hrsa():
+	hrsa = pd.read_csv('Data/grants_per_county_by_year.csv')
+	return hrsa
+
+hrsa = import_hrsa()
 
 # figure out difference for local vs cloud
 
 
 ######### SIDE BAR #########
+
 st.sidebar.markdown("""
-# Analyzing Susceptibility to Mental Health Issues
+## DS4A / Women 2021 - Team 2
+# """)
+
+st.sidebar.image('Images/logo.png')
+
+st.sidebar.markdown("""
+## Analyzing Susceptibility to Mental Health Issues
 """)
+
 # Outline Options for Sidebar
-section = st.sidebar.selectbox("Outline", ("Executive Summary", "Datasets",
+section = st.sidebar.selectbox("Navigation Bar", ("The Team", "Project Overview", "Datasets",
                                "Exploratory Data Analysis", "Methodology", "Findings and Recommendation", "Resources"))
 
 st.sidebar.markdown("""
-# DS4A Women 2021 - Team 2
-# Whitney Brooks (Executive)
-# Catherine Greenman (Practitioner)
-# Margot Herman (Practitioner)
-# Michell Li (Practitioner)
-# Chiu-Feng (Steph) Yap (Practitioner)
+### Team Members: 
+Whitney Brooks (Executive) |  Catherine Greenman (Practitioner)  |  Margot Herman (Practitioner) |  Michell Li (Practitioner)  |  Chiu-Feng (Steph) Yap (Practitioner)
 """)
 
+######### ABOUT TEAM 2 #########
+if section == "The Team":
 
-######### EXECUTIVE SUMMARY #########
-if section == "Executive Summary":
+	st.write("""
+
+	### Project
+	To learn more about how we analyzed the susceptibility of U.S. adults to mental health issues, check out the navigation bar on the left.
+
+	### The Team
+	##### [Chiu-Feng (Steph) Yap, MPH](https://linkedin.com/in/chiufengyap/)
+	🧠  Data Analyst at a startup, MPH in Epidemiology with Advanced Data Science Certificate
+
+	##### [Michell Li](https://www.linkedin.com/in/michell-li/)
+	🧠  Business Intelligence Analyst at Instacart
+
+	##### Margot Herman
+	🧠  Degree in Computer Science, Software Engineer at a startup
+
+	##### [Catherine Greenman](https://www.linkedin.com/in/csgreenman/)
+	🧠  Data Engineer, Bachelor’s in Computer Science from Columbia University
+
+	##### [Whitney Brooks](http://linkedin.com/in/hookedonbooks1930)
+	🧠  SVP of Product Management at Wells Fargo
+	""")
+
+######### PROJECT OVERVIEW #########
+if section == "Project Overview":
 
     st.title('Analyzing Susceptibility to Mental Health Issues')
 
     st.write('''
 	## Problem Statement
-	While the stigma around mental health has decreased over the years, many providers have seen a spike in cases related to “diseases of despair.” These include anxiety and depression, which often go untreated or lead sufferers to “self-medicate” with substances like drugs and alcohol. According to the Tufts Medical Center and One Mind at Work, depression alone accounts for about $44 billion in losses to workplace productivity. In 2019, national spending on mental health services totaled $225.1 billion and accounted for 5.5% of all health spending1. Furthermore, approximately 40% of Americans live in a designated mental health provider shortage area, which exacerbates the problem. Across the US, each state has discretionary funding allocated specifically for mental health. Sufficient funds and effective resource allocation are necessary for the diagnosis and treatment of mental health issues. Mental health issues are pervasive and, now more than ever, need to be better understood to address their causes and impacts in a meaningful way.
+	While the stigma around mental health has decreased over the years, many providers have seen a spike in cases related to “diseases of despair.” These include anxiety and depression, which often go untreated or lead sufferers to “self-medicate” with substances like drugs and alcohol. 
+	
+	According to the Tufts Medical Center and One Mind at Work, depression alone accounts for about $44 billion in losses to workplace productivity. In 2019, national spending on mental health services totaled $225.1 billion and accounted for 5.5% of all health spending. 
+	
+	Furthermore, approximately 40% of Americans live in a designated mental health provider shortage area, which exacerbates the problem. Across the US, each state has discretionary funding allocated specifically for mental health. Sufficient funds and effective resource allocation are necessary for the diagnosis and treatment of mental health issues. 	Adequate funding is a key factor in most leading implementation science frameworks. 
+	
+	When mental health issues left untreated, these challenges can have a negative effect on a person's economic solvency, leading to increased rates of homelessness and poverty, social isolation, deteriorating physical health and shorter life expectancy, and decreased profitability for employers and their shareholders due to lower employee efficiency. 
 
+	
 	## Objective
+
+	Mental health issues are pervasive and, now more than ever, need to be better understood to address their causes and impacts in a meaningful way.
+
 	The goal of this project is to identify factors that make individuals more susceptible to mental health issues, based on self-administered substance use, demographics, and geographic information from the National Survey on Drug Use and Health (NSDUH).
+	
+	## Problem Importance 
+	If we could identify the type of population who is more likely to struggle from mental health issues and identify features/variables features that we could potentially use to provide insights in terms of where the funding for mental health services should be distributed.
 	''')
 
 
 ######### DATASETS #########
 if section == "Datasets":
+	st.title('Datasets')
+	st.markdown('''
+	We leveraged three datasets from 2015-2019 for our statistical analysis:
+	''')
+	
+	st.write('''
+	### [National Survey on Drug Use and Health (NSDUH)](https://www.datafiles.samhsa.gov/dataset/national-survey-drug-use-and-health-2019-nsduh-2019-ds0001)
+	This dataset contains survey level information on the trends in specific substance use and mental illness measures''')
+	st.dataframe(nsduh.head(5))
 
-    st.title('Datasets')
+	st.write('''
+	### [Health Professional Shortage Areas (HPSA)](https://data.hrsa.gov/data/download#SHORT)
+	This datasets contains clinic metadata, clinic geospatial data, and clinic-level data including date of designation and withdrawal''')
+	st.dataframe(hpsa.head(5))
 
-    st.write(''' ### Top 10 rows of NSDUH dataset ''')
-    st.dataframe(df.head(10))
-    st.write('''
+	st.write('''
+	### [Health Resources and Services Administration (HRSA)](https://data.hrsa.gov/data/download)
+	This dataset contains total funding and mental-health related funding on a state and county-level. ''')
+	st.dataframe(hrsa.head(5))
+
+	st.write('## Data Cleaning')
+
+	st.write('''
+	#### NSDUH
+	The NSDUH dataset originally has 210, 959 records and more than 2000 columns. As the NSDUH consolidates location data into three overarching categories and does not preserve state or county-level data, we decided to consolidate five years’ worth of reports in order to use time as a metric for tracking trends in mental health indicators. The reports are yearly, from 2015 to 2019. 
+	
+	* Preserve only the columns relevant to our analysis (including answers to mental health screening questions, insurance status, and demographic data
+	* Consolidate non-committal answers (don’t know, refused, legitimate skip) into a single value
+	''')
+
+	st.write('''
+	#### HPSA
+	The HPSA dataset has 27,813 rows and 65 columns. Rows are on the HPSA entity level. The following was done to clean the data:
+	
+	* Remove variables with more than 30% data missing
+	* Remove variables without any variation (Break in Designation, Discipline Class Number, Data Warehouse Record Create Date Text)
+	* The resulting dataset has 27,813 rows and 44 columns.''')
+
+	st.write('''
+	#### HRSA Awarded Grants and CBSA
+	
+	HRSA Awarded Grants data contains awards from 2013 through 2021. For the Exploratory Data Analysis, we just decided to look at grants from 2015 to 2019. 
+	
+	* Only keep columns related to Financial Assistance, Award Year,  Grant Program Description, and geographic information like State and County names 
+	* Group by County and State and calculate the sum of the Financial Assistance as well as Financial Assistance related to mental health for all awards in a specific county from the HRSA dataset.
+	* Inner join HRSA with the CBSA dataset on State and County in order to relate Financial Assistance for awarded grants per county with the county population
+	''')
+	st.write('## Feature Engineering')
+	st.write('''
+	#### NSDUH
+	
+	HRSA Awarded Grants data contains awards from 2013 through 2021. For the Exploratory Data Analysis, we just decided to look at grants from 2015 to 2019. The first goal of feature engineering for the HPSA dataset is to create a column that allows the dataset to be joined with the NSDUH dataset. The second goal is to generate features using existing features that could enhance the dataset. 
+	
+	* Created a column for population density based on CBSA population density definitions for 2010. (PDEN10)
+	* Created Time spent as designated entity variable (DaysBeforeWithdrawn)
+	''')
+
+	st.markdown('''
 	## Outcome Variable
 	The outcome variable is captured in the data as a binary indicator of 1 (Yes) for ‘Past Month Serious Psychological Distress Indicator’ which is derived from a series of six questions, asking adults respondents how frequently they experienced the following symptoms in the past 30 days:
 
-		* How often did you feel nervous?
-		* How often did you feel hopeless?
-		* How often did you feel restless or fidgety?
-		* How often did you feel so sad/depressed that nothing could cheer you up?
-		* How often did you feel that everything was an effort?
-		* How often did you feel down on yourself, no good or worthless?
-
+	* How often did you feel nervous?
+	* How often did you feel hopeless?
+	* How often did you feel restless or fidgety?
+	* How often did you feel so sad/depressed that nothing could cheer you up?
+	* How often did you feel that everything was an effort?
+	* How often did you feel down on yourself, no good or worthless? 
+	
 	Questions are asked on a likert scale of 1-5, with a sum greater than 13 being the threshold for the outcome variable.
 	''')
-
-    st.write('''### Top 10 rows of the dataset''')
-
-
-    st.dataframe(df.head(10))
 
 
 ######### EXPLORATORY DATA ANALYSIS #########
 if section == "Exploratory Data Analysis":
 
     st.title('Exploratory Data Analysis')
-
-    st.markdown('''
-	## Outcome Variable
-
-	The outcome variable is captured in the data as a binary indicator of 1 (Yes) for ‘Past Month Serious Psychological Distress Indicator’ which is derived from a series of six questions, asking adults respondents how frequently they experienced the following symptoms in the past 30 days:
-
-		* How often did you feel nervous?
-		* How often did you feel hopeless?
-		* How often did you feel restless or fidgety?
-		* How often did you feel so sad/depressed that nothing could cheer you up?
-		* How often did you feel that everything was an effort?
-		* How often did you feel down on yourself, no good or worthless?
-
-	Questions are asked on a likert scale of 1-5, with a sum greater than 13 being the threshold for the outcome variable.
-	''')
-
-
-######### EXPLORATORY DATA ANALYSIS #########
-if section == "Exploratory Data Analysis":
-
-
-    st.title('Exploratory Data Analysis')
+	
 
 
 ######### METHODOLOGY #########
